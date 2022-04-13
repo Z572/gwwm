7f871d27779e9b63403b7779f57698fcc3baf1f5
Z572 <873216071@qq.com>
2022-04-13 20:39:09 +0800

    u

--- lisp/wlroots/types/compositor.scm
+++ lisp/wlroots/types/compositor.scm
@@ -1,21 +1,17 @@
 (define-module (wlroots types compositor)
   #:use-module (wayland display)
   #:use-module (wlroots render renderer)
+  #:use-module (wlroots types)
   #:use-module (wlroots utils)
   #:use-module (oop goops)
   #:export (wrap-wlr-compositor
             unwrap-wlr-compositor
             wlr-compositor-create))
 
-(define-class <wlr-compositor> ()
-  (pointer #:accessor .pointer #:init-keyword #:pointer))
+(define-wlr-types-class wlr-compositor)
 
-(define (wrap-wlr-compositor p)
-  (make <wlr-compositor> #:pointer p))
-(define (unwrap-wlr-compositor o)
-  (.pointer o))
-
-(define wlr-compositor-create
-  (let ((proc (wlr->procedure '* "wlr_compositor_create" '(* *))))
-    (lambda (display renderer)
-      (wrap-wlr-compositor (proc (unwrap-wl-display display) (unwrap-wlr-renderer renderer))))))
+(define-wlr-procedure (wlr-compositor-create display renderer)
+  ('* "wlr_compositor_create" '(* *))
+  (wrap-wlr-compositor
+   (% (unwrap-wl-display display)
+      (unwrap-wlr-renderer renderer))))
--- lisp/wlroots/types/data-device.scm
+++ lisp/wlroots/types/data-device.scm
@@ -1,6 +1,7 @@
 (define-module (wlroots types data-device)
   #:use-module (wayland display)
   ;;#:use-module (wlroots render renderer)
+  #:use-module (wlroots types)
   #:use-module (wlroots utils)
   #:use-module (oop goops)
   #:export (wrap-wlr-data-source
@@ -9,23 +10,12 @@
             unwrap-wlr-data-device-manager
             wlr-data-device-manager-create))
 
-(define-class <wlr-data-source> ()
-  (pointer #:accessor .pointer #:init-keyword #:pointer))
+(define-wlr-types-class
+  wlr-data-source)
 
-(define (wrap-wlr-data-source p)
-  (make <wlr-data-source> #:pointer p))
-(define (unwrap-wlr-data-source o)
-  (.pointer o))
+(define-wlr-types-class wlr-data-device-manager)
 
-(define-class <wlr-data-device-manager> ()
-  (pointer #:accessor .pointer #:init-keyword #:pointer))
 
-(define (wrap-wlr-data-device-manager p)
-  (make <wlr-data-device-manager> #:pointer p))
-(define (unwrap-wlr-data-device-manager o)
-  (.pointer o))
-
-(define wlr-data-device-manager-create
-  (let ((proc (wlr->procedure '* "wlr_data_device_manager_create" '(*))))
-    (lambda (display)
-      (wrap-wlr-data-device-manager (proc (unwrap-wl-display display) )))))
+(define-wlr-procedure (wlr-data-device-manager-create display)
+  ('* "wlr_data_device_manager_create" '(*))
+  (wrap-wlr-data-device-manager (% (unwrap-wl-display display))))
