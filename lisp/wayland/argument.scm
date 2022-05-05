867b078133e38c991def679809a8798304d6f13b
Z572 <873216071@qq.com>
2022-05-05 22:40:23 +0800

    wayland/argument.scm: add module and export %wl-argument-struct.
    lisp/wayland/argument.scm:
        add define-module.
        rename %wl-argument to %wl-argument-struct.
        export %wl-argument-struct.

--- lisp/wayland/argument.scm
+++ lisp/wayland/argument.scm
@@ -1,9 +1,13 @@
-
-(define %wl-argument (bs:union `((i ,ffi:int32)
-                                 (u ,ffi:uint32)
-                                 (f ,ffi:int32)
-                                 (s ,cstring-pointer)
-                                 (o ,(bs:pointer '*)) ;; wl_object *
-                                 (n ,ffi:uint32)
-                                 (a ,(bs:pointer '*)) ;; wl_array *
-                                 (h ,ffi:int32))))
+(define-module (wayland argument)
+  #:use-module (bytestructures guile)
+  #:use-module ((system foreign) #:prefix ffi:)
+  #:export (%wl-argument-struct))
+(define %wl-argument-struct
+  (bs:union `((i ,ffi:int32)
+              (u ,ffi:uint32)
+              (f ,ffi:int32)
+              (s ,cstring-pointer)
+              (o ,(bs:pointer '*)) ;; wl_object *
+              (n ,ffi:uint32)
+              (a ,(bs:pointer '*)) ;; wl_array *
+              (h ,ffi:int32))))
