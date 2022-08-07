9d16ec34f96f39348aa1fb690918c4b3b23eab6e
Z572 <873216071@qq.com>
2022-08-07 12:56:11 +0800

    add client-is-floating? procedure.

--- gwwm.c
+++ gwwm.c
@@ -133,6 +133,15 @@ SCM_DEFINE (gwwm_client_is_fullscreen_p, "client-is-fullscreen?" ,1,0,0,
 }
 #undef FUNC_NAME
 
+SCM_DEFINE (gwwm_client_is_floating_p, "client-is-floating?" ,1,0,0,
+            (SCM c), "")
+#define FUNC_NAME s_gwwm_client_is_floating_p
+{
+  Client *cl = scm_foreign_object_ref(c, 0);
+  return scm_from_bool(cl->isfloating);
+}
+#undef FUNC_NAME
+
 typedef struct {
 	uint32_t singular_anchor;
 	uint32_t anchor_triplet;
