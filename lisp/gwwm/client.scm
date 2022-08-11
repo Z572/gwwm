7fb3714cfac74aaa11a59f45d00c2bc0f2f41e3e
Z572 <873216071@qq.com>
2022-08-11 23:40:43 +0800

    gwwm/client.scm: add client-{is-fullscreen?,fullscreen?,set-fullscreen!} procedure.

--- lisp/gwwm/client.scm
+++ lisp/gwwm/client.scm
@@ -2,7 +2,10 @@
   #:export (current-client
             client-floating?
             client-set-floating
-            client-is-floating?))
+            client-is-floating?
+            client-fullscreen?
+            client-is-fullscreen?
+            client-set-fullscreen!))
 
 (define (current-client)
   ((@@ (gwwm) current-client)))
@@ -13,7 +16,18 @@
 (define (client-set-floating client floating?)
   ((@@ (gwwm) client-set-floating) client floating? ))
 
+(define (client-is-fullscreen? client)
+  ((@@ (gwwm) client-is-fullscreen?) client))
+
+(define (client-set-fullscreen! client fullscreen?)
+  ((@@ (gwwm) client-set-fullscreen!) client fullscreen? ))
+
 (define client-floating?
   (make-procedure-with-setter
    client-is-floating?
    client-set-floating))
+
+(define client-fullscreen?
+  (make-procedure-with-setter
+   client-is-fullscreen?
+   client-set-fullscreen!))
