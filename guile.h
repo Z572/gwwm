9584e45ae7d6bf2d59b88390f621d1f438511ea2
Z572 <873216071@qq.com>
2022-08-19 19:57:03 +0800

    guile.h: fix UNWRAP_WLR_XDG_SURFACE macro.
    guile.h: (UNWRAP_WLR_XDG_SURFACE): use unwrap replace wrap.

--- guile.h
+++ guile.h
@@ -29,7 +29,7 @@
 #define WRAP_WLR_XDG_SURFACE(p)                                                    \
   (REF_CALL_1("wlroots types xdg-shell", "wrap-wlr-xdg-surface", FROM_P(p)))
 #define UNWRAP_WLR_XDG_SURFACE(p)                                                  \
-  (TO_P(REF_CALL_1("wlroots types xdg-shell", "wrap-wlr-xdg-surface", p)))
+  (TO_P(REF_CALL_1("wlroots types xdg-shell", "unwrap-wlr-xdg-surface", p)))
 #define WRAP_WLR_XWAYLAND_SURFACE(p)                                                    \
   (REF_CALL_1("wlroots xwayland", "wrap-wlr-xwayland-surface", FROM_P(p)))
 #define UNWRAP_WLR_XWAYLAND_SURFACE(p)                                                  \
