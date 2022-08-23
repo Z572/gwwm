42fe4735c9055a12363b575f4f4bb266f29764e9
Z572 <873216071@qq.com>
2022-08-23 19:22:23 +0800

    utils.scm: add ->string procedure.
    lisp/gwwm/utils.scm: (->string) new procedure.

--- lisp/gwwm/utils.scm
+++ lisp/gwwm/utils.scm
@@ -1,10 +1,27 @@
 (define-module (gwwm utils)
   #:use-module (oop goops)
-  #:export (->symbol))
+  #:export (->symbol
+            ->string))
 
 (define-method (->symbol (o <symbol>))
   o)
 (define-method (->symbol (o <number>))
-  (string->symbol (number->string o)))
+  (->symbol (number->string o)))
+
 (define-method (->symbol (o <string>))
   (string->symbol o))
+
+(define-method (->symbol (o <keyword>))
+  (keyword->symbol o))
+
+(define-method (->string (o <string>))
+  o)
+
+(define-method (->string (o <keyword>))
+  (->string (keyword->symbol o)))
+
+(define-method (->string (o <symbol>))
+  (symbol->string o))
+
+(define-method (->string (o <number>))
+  (number->string o))
