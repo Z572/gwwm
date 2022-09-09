acd4464383e3d2018da8b5dc1229e86c0627c482
Z572 <873216071@qq.com>
2022-09-09 22:50:07 +0800

    u

--- gwwm/utils.scm
+++ gwwm/utils.scm
@@ -1,7 +1,16 @@
 (define-module (gwwm utils)
   #:use-module (oop goops)
+  #:use-module (oop goops describe)
+  #:use-module (ice-9 format)
+  #:use-module (gwwm i18n)
   #:export (->symbol
             ->string))
+
+(define-method (describe (m <hashtable>))
+  (format #t (G_ "~S is a hashtable, value is:~%~:{\t~s => ~s\n~}.~%")
+          m (hash-map->list
+             list m))
+  *unspecified*)
 
 (define-method (->symbol (o <symbol>))
   o)
