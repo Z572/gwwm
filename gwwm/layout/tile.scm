ee26db52b48de4d1b61eb9ca761e2e0d859529f8
Z572 <873216071@qq.com>
2023-01-20 23:36:52 +0800

    gwwm/layout/tile.scm: tile: improve.

--- gwwm/layout/tile.scm
+++ gwwm/layout/tile.scm
@@ -2,6 +2,8 @@
   #:use-module (oop goops)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-2)
+  #:use-module (srfi srfi-71)
+  #:use-module (ice-9 match)
   #:use-module (wlroots util box)
   #:use-module (util572 box)
   #:use-module (gwwm monitor)
@@ -9,41 +11,35 @@
   #:use-module (gwwm layout)
   #:export (tile-layout))
 (define (tile m)
-  (and-let* ((mfact (monitor-mfact m))
-             (nmaster (monitor-nmaster m))
-
-             (clients (filter (lambda (c)
+  (and-let* ((clients (filter (lambda (c)
                                 (and (visibleon c m)
                                      (not (client-floating? c))
                                      (not (client-fullscreen? c))))
                               (client-list)))
              (l (length clients))
-             ((not (zero? l))))
-    (call-with-values (lambda () (split-at clients (min nmaster l)))
-      (lambda (nm others)
-        (let* ((nmaster-width (if (zero? (length others))
-                                  (box-width (monitor-window-area m))
-                                  (* (box-width (monitor-window-area m)) mfact)))
-               (nmaster-height (round (/ (box-height (monitor-window-area m)) (length nm))))
-               (nmaster-boxs (map (lambda (y) (make-wlr-box 0 y nmaster-width nmaster-height))
-                                  (iota (length nm)
-                                        0
-                                        (round (/ (box-height (monitor-window-area m))
-                                                  (length nm)))))))
-          (for-each (lambda (c box)
-                      (client-resize c box)) clients
-                      (if (zero? (length others))
-                          nmaster-boxs
-                          (append nmaster-boxs
-                                  (let* ((other-height (round (/ (box-height (monitor-window-area m))
-                                                                 (length others))))
-                                         (other-width (- (box-width (monitor-window-area m)) nmaster-width)))
-                                    (map (lambda (y) (make-wlr-box
-                                                      nmaster-width
-                                                      y
-                                                      other-width
-                                                      other-height ))
-                                         (iota (length others) 0 other-height)))))))))))
+             ((not (zero? l)))
+             (mfact (monitor-mfact m))
+             (nmaster (monitor-nmaster m))
+             (window-box (monitor-window-area m))
+             (b (if (< nmaster l)
+                    (round (* mfact (box-width window-box)))
+                    (box-width window-box))))
+    (let ((nmaster-clients other-clients (split-at clients (min nmaster l))))
+      (match (split-box window-box b 'y)
+        ((nmaster-box other-box)
+         (let ((p (lambda (cls box)
+                    (unless (zero? (length cls))
+                      (for-each (lambda (c box)
+                                  (client-resize c box))
+                                cls
+                                (split-box/n
+                                 box
+                                 (round
+                                  (/ (box-height window-box)
+                                     (length cls))) 'x))))))
+           (p nmaster-clients nmaster-box)
+           (p other-clients other-box)))))))
+
 (define tile-layout
   (make <layout>
     #:symbol "[t]"
