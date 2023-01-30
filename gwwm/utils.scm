b6d1d98e8d0a47957b81aad1096f7ba5cbb0f456
Z572 <873216071@qq.com>
2023-01-30 18:34:01 +0800

    remove string-split-length.

--- gwwm/utils.scm
+++ gwwm/utils.scm
@@ -94,12 +94,6 @@
              (let ((out (begin sexp ...)))
                (set! slot-name out)) ...))))))
 
-(define* (string-split-length s length)
-  (let loop ((s s))
-    (if (> (string-length s ) length)
-        (cons (string-take s length) (loop (string-drop s length)))
-        (list s))))
-
 ;;; copy from guix.
 (define-syntax-rule (save-environment-excursion body ...)
   "Save the current environment variables, run BODY..., and restore them."
