3fc620bdfcd1334781ccfdfbbed5e2d88456153c
Z572 <873216071@qq.com>
2023-02-01 22:55:25 +0800

    gwwm/packages/fullscreen-bg.scm: fix.

--- gwwm/packages/fullscreen-bg.scm
+++ gwwm/packages/fullscreen-bg.scm
@@ -35,7 +35,7 @@
 (define (add-bg/maybe c)
   (unless (client-bg c)
     (let ((bg (wlr-scene-rect-create
-               (.node (client-scene c))
+               (client-scene c)
                (box-width (client-geom c))
                (box-height (client-geom c))
                (fullscreen-bg-color))))
