9cba4c8a12a4e8c48f15ad157836e850bb59821e
Z572 <873216071@qq.com>
2023-02-02 21:28:19 +0800

    gwwm/listener.scm: add-listen: use wl-listener-remove.

--- gwwm/listener.scm
+++ gwwm/listener.scm
@@ -4,6 +4,7 @@
   #:use-module (wayland signal)
   #:use-module (wayland list)
   #:use-module (wayland listener)
+  #:use-module (gwwm utils srfi-215)
   #:use-module (oop goops)
   #:export (add-listen))
 
@@ -12,10 +13,12 @@
                      (destroy-when obj)
                      (remove-when-destroy? #t))
   (let ((listener (make-wl-listener proc)))
+    (send-log DEBUG "listener added" 'object obj 'event symbol)
     (wl-signal-add (get-event-signal obj symbol) listener)
     (when remove-when-destroy?
       (wl-signal-add
        (get-event-signal destroy-when 'destroy)
        (make-wl-listener
         (lambda _
-          (wl-list-remove (.link listener))))))))
+          (send-log DEBUG "listener removed" 'object obj 'event symbol)
+          (wl-listener-remove listener)))))))
