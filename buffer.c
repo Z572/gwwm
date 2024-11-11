426609aede3a03e848f580fa2699636ca7cc1f28
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-11 20:02:10 +0800

    u

--- buffer.c
+++ buffer.c
@@ -18,11 +18,19 @@
 struct gwwm_buffer {
   struct wlr_buffer base;
   SCM scm;
+  SCM surface;
+  SCM cairo;
+  SCM format;
 };
 
 static void gwwm_buffer_destroy(struct wlr_buffer *wlr_buffer) {
   struct gwwm_buffer *buffer = wl_container_of(wlr_buffer, buffer, base);
   buffer->scm = NULL;
+  scm_gc_unprotect_object(buffer->cairo);
+  buffer->cairo = NULL;
+  scm_gc_unprotect_object(buffer->surface);
+  buffer->surface=NULL;
+  buffer->format=NULL;
   free(buffer);
 }
 static bool gwwm_buffer_begin_data_ptr_access(struct wlr_buffer *wlr_buffer,
@@ -30,15 +38,12 @@ static bool gwwm_buffer_begin_data_ptr_access(struct wlr_buffer *wlr_buffer,
                                               uint32_t *format,
                                               size_t *stride) {
   struct gwwm_buffer *buffer = wl_container_of(wlr_buffer, buffer, base);
-  SCM ssurface =
-      (scm_slot_ref(buffer->scm, scm_from_utf8_symbol("image-surface")));
-
+  SCM ssurface =buffer->surface;
   *data = (TO_P(REF_CALL_1(
       "system foreign", "bytevector->pointer",
       (REF_CALL_1("cairo", "cairo-image-surface-get-data", ssurface)))));
 
-  *format = (scm_to_uint32(
-      scm_slot_ref(buffer->scm, scm_from_utf8_symbol("format"))));
+  *format = (scm_to_uint32(buffer->format));
   *stride = (scm_to_double(
       REF_CALL_1("cairo", "cairo-image-surface-get-stride", ssurface)));
   return true;
@@ -62,35 +67,40 @@ static struct gwwm_buffer *gwwm_buffer_create(int width, int height) {
                             scm_from_int(height));
   buffer->scm = (scm_call_3(
       REF("oop goops", "make"), REFP("gwwm buffer", "<gwwm-cairo-buffer>"),
-      scm_from_utf8_keyword("data"), scm_pointer_address(FROM_P(buffer))));
-  scm_slot_set_x(buffer->scm, scm_from_utf8_symbol("cairo"),
-                 REF_CALL_1("cairo", "cairo-create", ssurface));
-  scm_slot_set_x(buffer->scm, scm_from_utf8_symbol("image-surface"), ssurface);
-  scm_slot_set_x(buffer->scm, scm_from_utf8_symbol("format"),
-                 scm_from_unsigned_integer(DRM_FORMAT_ARGB8888));
-  wlr_buffer_lock(&buffer->base);
-  wlr_buffer_drop(&buffer->base);
+      scm_from_utf8_keyword("data"), FROM_P(buffer)));
+  buffer->cairo=REF_CALL_1("cairo", "cairo-create", ssurface);
+  buffer->surface=ssurface;
+  buffer->format=scm_from_unsigned_integer(DRM_FORMAT_ARGB8888);
+  scm_gc_protect_object(ssurface);
+  scm_gc_protect_object(buffer->cairo);
   return buffer;
 }
+
 SCM_DEFINE(cairo_buffer_create, "cairo-buffer-create", 2, 0, 0,
            (SCM width, SCM height), "") {
   return (gwwm_buffer_create(scm_to_int(width), scm_to_int(height)))->scm;
 }
 
 SCM_DEFINE(cairo_buffer_base, "cairo-buffer-base", 1, 0, 0, (SCM buffer), "") {
-  return WRAP_WLR_BUFFER(&((struct gwwm_buffer *)(TO_P(MAKE_P(scm_slot_ref(
-                               buffer, scm_from_utf8_symbol("%data"))))))
+  return WRAP_WLR_BUFFER(&((struct gwwm_buffer *)(TO_P(scm_slot_ref(
+                               buffer, scm_from_utf8_symbol("%data")))))
                               ->base);
 }
+
+SCM_DEFINE(cairo_buffer_cairo, "cairo-buffer-cairo", 1, 0, 0, (SCM buffer), "") {
+  return ((struct gwwm_buffer *)(TO_P(scm_slot_ref(
+                               buffer, scm_from_utf8_symbol("%data")))))->cairo;
+}
+
 SCM_DEFINE(set_cairo_buffer_base, "set-cairo-buffer-base!", 2, 0, 0,
            (SCM buffer, SCM wlr_buffer), "") {
   ((struct gwwm_buffer *)(TO_P(
-       MAKE_P(scm_slot_ref(buffer, scm_from_utf8_symbol("%data"))))))
+       scm_slot_ref(buffer, scm_from_utf8_symbol("%data")))))
       ->base = *((struct wlr_buffer *)(UNWRAP_WLR_BUFFER(wlr_buffer)));
   return SCM_UNSPECIFIED;
 }
-
 void scm_init_gwwm_buffer(void) {
+  scm_make_foreign_object_type
 #ifndef SCM_MAGIC_SNARFER
 #include "buffer.x"
 #endif
--- gwwm/buffer.scm
+++ gwwm/buffer.scm
@@ -2,48 +2,59 @@
   #:use-module (system foreign-object)
   #:use-module (cairo)
   #:use-module (oop goops)
+  #:use-module (system foreign)
+  #:use-module (wlroots types)
+  #:use-module (wlroots types buffer)
+  #:use-module (wayland signal)
+  #:use-module (wayland list)
+  #:use-module (wayland server listener)
+  #:use-module (gwwm utils srfi-215)
+  #:use-module (oop goops)
+
   #:export (cairo-buffer-create
             cairo-buffer-base
-            cairo-buffer-cairo
-            .cairo))
+            cairo-buffer-cairo))
 
-;; (use-modules (gwwm buffer)
-;;              (oop goops describe)
-;;              (gwwm client)
-;;              (cairo)
-;;              (wlroots types scene))
-;; (define buf (cairo-buffer-create 30 40))
-;; (define cr (.cairo buf))
-;; (define target (cairo-get-target cr))
-;; (cairo-set-source-rgba cr 1.0 0.4 1.0 1.0)
-;; (cairo-set-fill-rule cr 'winding)
-;; (cairo-rectangle cr 0 0 15 40)
-;; (cairo-fill cr)
-;; (cairo-set-source-rgba cr 0.4 1.0 1.0 1.0)
-;; (cairo-rectangle cr 4 0 30 20)
-;; (cairo-fill cr)
-;; (cairo-surface-flush target)
-;; (spawn "emacs" '("emacs"))
-;; (sleep 2)
-;; (wlr-scene-buffer-create (client-scene (car (client-list)))
-;;                          (cairo-buffer-base buf))
+(use-modules (gwwm buffer)
+             (oop goops describe)
+             (gwwm client)
+             (cairo)
+             (wlroots types scene))
 
-
-
+;; (define (test)
+;;   (let* ((buf (cairo-buffer-create 30 40))
+;;          (cr (cairo-buffer-cairo buf))
+;;          (target (cairo-get-target cr)))
+;;     (cairo-set-source-rgba cr 1.0 0.4 1.0 1.0)
+;;     (cairo-set-fill-rule cr 'winding)
+;;     (cairo-rectangle cr 0 0 15 40)
+;;     (cairo-fill cr)
+;;     (cairo-set-source-rgba cr 0.4 1.0 1.0 1.0)
+;;     (cairo-rectangle cr 4 0 30 20)
+;;     (cairo-fill cr)
+;;     (cairo-surface-flush target)
+;;     (letrec* ((sc-b (wlr-scene-buffer-create (client-scene (car (client-list)))
+;;                                              (cairo-buffer-base buf)))
+;;               (node (.node sc-b))
+;;               (listener (make-wl-listener
+;;                          (lambda _
+;;                            (wlr-buffer-drop (cairo-buffer-base buf))
+;;                            (wl-listener-remove listener)))))
+;;       (wl-signal-add (get-event-signal node 'destroy)
+;;                      listener))))
 
 (eval-when (expand load eval)
   (load-extension "libgwwm" "scm_init_gwwm_buffer"))
-(define-class <gwwm-cairo-buffer> ()
-  (%data #:init-keyword #:data
-         ;; #:accessor .data
-         #:class <hidden-slot>)
-  (buffer
-   #:allocation
-   #:virtual
-   #:slot-ref cairo-buffer-base
-   #:slot-set! set-cairo-buffer-base!
-   #:accessor .buffer)
-  (image-surface #:accessor .image-surface)
 
-  (cairo #:accessor .cairo)
-  (format #:accessor .format))
+;; (define DRM_FORMAT_ARGB8888 875713089)
+
+;; (define (cairo-buffer-create width height)
+;;   (let* ((surface (cairo-image-surface-create 'argb32 width height))
+;;          (cairo (cairo-create surface)))
+;;     (make <gwwm-cairo-buffer>
+;;       #:image-surface surface
+;;       #:cairo cairo
+;;       #:format DRM_FORMAT_ARGB8888)))
+
+(define-class <gwwm-cairo-buffer> ()
+  (%data #:init-keyword #:data))
