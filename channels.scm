37596a6b31c3dc150367ed8803301b08dc046dcb
Zheng Junjie <z572@z572.online>
2025-05-18 11:20:35 +0800

    channels.scm: Add guile-wayland's introduction.

--- channels.scm
+++ channels.scm
@@ -3,4 +3,9 @@
        (url "https://codeberg.org/guix/guix-mirror"))
       (channel
        (name 'guile-wayland)
+       (introduction
+        (make-channel-introduction
+         "9e4433fe570d2b74caee0182a2929e4a35ba59fb"
+         (openpgp-fingerprint
+          "7EBE A494 60CE 5E2C 0875  7FDB 3B5A A993 E1A2 DFF0")))
        (url "https://github.com/guile-wayland/channel")))
